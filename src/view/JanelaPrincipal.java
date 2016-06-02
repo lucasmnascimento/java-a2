ecfbdebeb4b6907db35da2b638fd34d622a9af1a
Rafael Oliveira <rafa_so@aol.com>
2016-06-02 07:19:02 -0300

    nothing

--- src/view/JanelaPrincipal.java
+++ src/view/JanelaPrincipal.java
@@ -1,69 +1,42 @@
 package view;
 
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.*;
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class JanelaPrincipal extends JFrame {
 
 	public JanelaPrincipal() {
 		
-		super ("ReNATO bOY");
-		setSize (600,600);
+		super ("TRABALHO JAVA A2");
 		
-		//cria barra de menu
-		JMenuBar barra = new JMenuBar();
-		super.setJMenuBar (barra);
+		JPanel panel1 = new JPanel();
+		panel1.setLayout (new GridLayout (3, 2));
+	    
+	    JTextField textFieldNome = new JTextField (15);
+	    JTextField textFieldSobrenome = new JTextField (15);
 		
-		//cria um menu
-		JMenu menuObjeto = new JMenu ("Cadastro");
-		barra.add (menuObjeto);
-		
-		JMenuItem criar = new JMenuItem ("Inscrição");
-		JMenuItem alterar = new JMenuItem ("Alterar");
-		JMenuItem carregar = new JMenuItem ("Listar");
-		JMenuItem excluir = new JMenuItem ("Excluir");
-		JMenuItem sair = new JMenuItem ("Sair");
-		
-		menuObjeto.add (criar);
-		menuObjeto.add (alterar);
-		menuObjeto.add (carregar);
-		menuObjeto.add (excluir);
-		menuObjeto.addSeparator();
-		menuObjeto.add (sair);
-		
-		//nota: adicionar maneira de focar a janela
-		criar.addActionListener (new ActionListener() {
-			public void actionPerformed (ActionEvent e) {
-					DialogCria dialog = new DialogCria();
-			}
-		});
-		
-		sair.addActionListener (new ActionListener() {
-			public void actionPerformed (ActionEvent e) {
-				int confirma = JOptionPane.showConfirmDialog (null, "Você tem certeza que deseja sair do programa?", "Sair", JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
+	    panel1.add (new JLabel ("Nome: "));
+        panel1.add (textFieldNome);
+        
+        panel1.add (new JLabel ("Sobrenome"));
+        panel1.add (textFieldSobrenome);
+        
+        panel1.add (new JButton("Salvar"));
+        
+        add (panel1);
+        //add(panel2);
 
-				if (confirma == JOptionPane.YES_OPTION)
-					dispose();
-			}
-		});
+        pack();
+		setLocationRelativeTo (null);
 		
-		//bug ao sair da janela, cancelar também fecha...
-		addWindowListener (new WindowAdapter() {
-			public void windowClosing (WindowEvent e) {
-				int confirma = JOptionPane.showConfirmDialog (null,	"Você tem certeza que deseja sair do programa?", "Sair", JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
-
-				if (confirma == JOptionPane.YES_OPTION)
-					dispose();
-			}
-		});
-		
-		super.setDefaultCloseOperation (EXIT_ON_CLOSE);
-		
-		//setVisible é colocado aqui porque antes do menu, o mesmo não fica visível até redimensionarmos a janela
+		setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
 		setVisible (true);
 	}
 	
 	public static void main (String args[]) {
-		JanelaPrincipal jt = new JanelaPrincipal();
+		new JanelaPrincipal();
 	}
 }
